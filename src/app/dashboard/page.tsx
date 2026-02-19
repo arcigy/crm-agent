c3d2671603fed72f6e79c699ed412365b02415af
arcigy <automatizaciebackhand@gmail.com>
2026-02-19 09:10:06 +0100

    mobile(dashboard): fix layout, scrolling and overlapping issues

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -56,7 +56,7 @@ export default async function DashboardPage() {
   const activeTools = new Set(tools.map((t) => t.id));
 
   return (
-    <div className="h-[calc(100vh-140px)] md:h-[calc(100vh-40px)] flex flex-col max-w-full mx-auto overflow-hidden gap-4">
+    <div className="h-auto md:h-[calc(100vh-40px)] flex flex-col max-w-full mx-auto overflow-y-auto md:overflow-hidden gap-6 p-4 md:p-0">
       <PaymentSuccessToast />
 
       {/* Primary Stats */}
@@ -65,11 +65,11 @@ export default async function DashboardPage() {
       </div>
 
       {/* Main Operations Grid */}
-      <div className="flex-1 min-h-0 grid grid-cols-1 lg:grid-cols-2 grid-rows-2 gap-4 pb-2">
-        <div className="min-h-0"><TodoListWidget tasks={tasks} mode="today" /></div>
-        <div className="min-h-0"><ChartsRow deals={deals} projects={projects} /></div>
-        <div className="min-h-0"><AnalyticsSection contacts={contacts} deals={deals} projects={projects} /></div>
-        <div className="min-h-0"><CalendarWidget events={calendarEvents} /></div>
+      <div className="flex-1 min-h-0 grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 gap-6 pb-4">
+        <div className="min-h-[400px] lg:min-h-0"><TodoListWidget tasks={tasks} mode="today" /></div>
+        <div className="min-h-[400px] lg:min-h-0"><ChartsRow deals={deals} projects={projects} /></div>
+        <div className="min-h-[400px] lg:min-h-0"><AnalyticsSection contacts={contacts} deals={deals} projects={projects} /></div>
+        <div className="min-h-[400px] lg:min-h-0"><CalendarWidget events={calendarEvents} /></div>
       </div>
 
       <div className="hidden">
